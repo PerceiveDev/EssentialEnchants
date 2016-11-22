bb4561c3436f7704e24b9abb4c4a12186fd8be8c
Rayzr522 <rayzr522@gmail.com>
2016-11-22 08:06:29 -0500

    Project 'HydrusEnchants' generated
    Generated using PerceivePluginArchetype and custom CLI project generator tool by Rayzr

---
+++ src/main/java/com/perceivedev/hydrusenchants/HydrusEnchants.java
@@ -0,0 +1,12 @@
+package com.perceivedev.hydrusenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HydrusEnchants extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+}
